ba260dcdec5988eef8a7d3967ad0c730829daddf
jvm750 <jomavazquezme@ittepic.edu.mx>
2025-03-12 15:24:40 -0700

    UPT servidor de node y express completado, con base de datos integrada

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('¡Backend funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
