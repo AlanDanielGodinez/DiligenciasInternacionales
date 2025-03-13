1702cfdfb8042afd346c208094c9fee238b6d659
jvm750 <jomavazquezme@ittepic.edu.mx>
2025-03-13 11:50:22 -0700

    ADD componente carrucel

---
+++ diligenciasinternacionalesinc/src/Components/Carrucel.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Carousel.css'; // Importamos el CSS
+
+const Carousel = ({ images }) => {
+  return (
+    <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div className="carousel-indicators">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            type="button"
+            data-bs-target="#carouselExampleDark"
+            data-bs-slide-to={index}
+            className={index === 0 ? 'active' : ''}
+            aria-current={index === 0 ? 'true' : 'false'}
+            aria-label={`Slide ${index + 1}`}
+          ></button>
+        ))}
+      </div>
+      <div className="carousel-inner">
+        {images.map((image, index) => (
+          <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`} data-bs-interval={index === 0 ? '10000' : '2000'}>
+            <img src={image.src} className="d-block w-100" alt={image.alt} />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>{image.title}</h5>
+              <p>{image.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
