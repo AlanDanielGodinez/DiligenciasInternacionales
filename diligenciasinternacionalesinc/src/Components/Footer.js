48f8f95afc30b7c0ec8eec2c4bcb879e6d4fc8fb
jvm750 <jomavazquezme@ittepic.edu.mx>
2025-03-19 15:52:12 -0700

    UPT Footer completado al 90%

--- diligenciasinternacionalesinc/src/Components/Footer.js
+++ diligenciasinternacionalesinc/src/Components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { FaFacebook, FaTiktok, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'; // Importa los iconos de redes sociales
 
 const Footer = () => {
   return (
@@ -8,14 +9,14 @@ const Footer = () => {
         <div className="footer-section">
           <h3 className="footer-title">Sobre Nosotros</h3>
           <p className="footer-text">
-          Somos un corporativo que nace en el 2017 con el objetivo de lograr que más familias se puedan <br></br> volver a encontrar.
+            Somos una empresa dedicada a ofrecer soluciones innovadoras para nuestros clientes.
           </p>
         </div>
         <div className="footer-section">
           <h3 className="footer-title">Enlaces Rápidos</h3>
           <ul className="footer-links">
             <li><a href="/">Inicio</a></li>
-            <li><a href="/servicios">Sobre nosotros</a></li>
+            <li><a href="/servicios">Servicios</a></li>
             <li><a href="/contacto">Contacto</a></li>
           </ul>
         </div>
@@ -25,6 +26,23 @@ const Footer = () => {
             Email: info@empresa.com<br />
             Teléfono: +123 456 789
           </p>
+        </div>
+        <div className="footer-section">
+          <h3 className="footer-title">Síguenos</h3>
+          <div className="social-icons">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FaFacebook className="icon" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <FaTiktok className="icon" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <FaInstagram className="icon" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="icon" />
+            </a>
+          </div>
         </div>
       </div>
       <div className="footer-bottom">
